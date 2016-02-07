fb250c62e63447eb5b7dc3a87fef7f648d90b672
ulisesmx <mx.ulises@gmail.com>
2016-02-07 12:05:45 -0600

    07-02-2016: Adding the unary operators classes to the CTL module

--- lib/ctl.py
+++ lib/ctl.py
@@ -27,11 +27,12 @@
 ================================================================================
 
       Version: 0.1
-  Last Update: 06-02-2016
+  Last Update: 07-02-2016
 
   Date        Alias      Description
 --------------------------------------------------------------------------------
   06-02-2016  ulisesma   Initial file creation
+  07-02-2016  ulisesma   Adding the unary operators classes
 
 """
 
@@ -88,7 +89,7 @@ class CTLTrue(CTLFormula):
 
 
 class CTLFalse(CTLFormula):
-    """ Class to represent the True operator """
+    """ Class to represent the False operator """
 
     def __init__(self):
         self._operator = "FALSE"
@@ -104,7 +105,7 @@ class CTLFalse(CTLFormula):
 
 
 class CTLAtomicProposition(CTLFormula):
-    """ Class to represent the True operator """
+    """ Class to represent the Atomic Proposition operator """
 
     def __init__(self, place):
         if not isinstance(place, basestring):
@@ -124,7 +125,7 @@ class CTLAtomicProposition(CTLFormula):
 
 
 class CTLNegatedAtomicProposition(CTLFormula):
-    """ Class to represent the True operator """
+    """ Class to represent the Negated Atomic Proposition operator """
 
     def __init__(self, place):
         if not isinstance(place, basestring):
@@ -141,3 +142,96 @@ class CTLNegatedAtomicProposition(CTLFormula):
     def negate(self):
         """ Negate the current CTL formula """
         return CTLAtomicProposition(self._place)
+
+
+"""
+================================================================================
+                              UNARY OPERATORS
+================================================================================
+
+This section includes the following CTL unary operators:
+    * Exist Next
+    * Negated Exist Next
+    * Exist Globally
+    * Negated Exist Globally
+"""
+
+
+class CTLExistNext(CTLFormula):
+    """ Class to represent the Exist Next operator """
+
+    def __init__(self, phi):
+        if not isinstance(phi, CTLFormula):
+            err_message = "Phi provided is not an CTL formula"
+            raise CTLException(err_message)
+        self._phi = phi
+        LOG.info("New exist next predicate created")
+
+
+    def print_lola(self):
+        return "EX({0})".format(self.phi.print_lola())
+
+
+    def negate(self):
+        """ Negate the current CTL formula """
+        return CTLNegatedExistNext(self.phi)
+
+
+class CTLNegatedExistNext(CTLFormula):
+    """ Class to represent the Negated Exist Next operator """
+
+    def __init__(self, phi):
+        if not isinstance(phi, CTLFormula):
+            err_message = "Phi provided is not an CTL formula"
+            raise CTLException(err_message)
+        self._phi = phi
+        LOG.info("New negated exist next predicate created")
+
+
+    def print_lola(self):
+        return "NOT EX({0})".format(self.phi.print_lola())
+
+
+    def negate(self):
+        """ Negate the current CTL formula """
+        return CTLExistNext(self.phi)
+
+
+class CTLExistGlobally(CTLFormula):
+    """ Class to represent the Exist Globally operator """
+
+    def __init__(self, phi):
+        if not isinstance(phi, CTLFormula):
+            err_message = "Phi provided is not an CTL formula"
+            raise CTLException(err_message)
+        self._phi = phi
+        LOG.info("New exist globally predicate created")
+
+
+    def print_lola(self):
+        return "EG({0})".format(self.phi.print_lola())
+
+
+    def negate(self):
+        """ Negate the current CTL formula """
+        return CTLNegatedExistGlobally(self.phi)
+
+
+class CTLNegatedExistGlobally(CTLFormula):
+    """ Class to represent the Negated Exist Globally operator """
+
+    def __init__(self, phi):
+        if not isinstance(phi, CTLFormula):
+            err_message = "Phi provided is not an CTL formula"
+            raise CTLException(err_message)
+        self._phi = phi
+        LOG.info("New negated exist globally predicate created")
+
+
+    def print_lola(self):
+        return "NOT EG({0})".format(self.phi.print_lola())
+
+
+    def negate(self):
+        """ Negate the current CTL formula """
+        return CTLExistGlobally(self.phi)
